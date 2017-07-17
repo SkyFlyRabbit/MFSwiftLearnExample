dc091b7377a3f065d9c68949213347ffa317105e
xumengfan <xmf@xkeshi.com>
2017-07-17 09:11:14 +0800

    Initial Commit

---
+++ MFSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MFSwiftExample
+//
+//  Created by XuMengFan on 2017/7/17.
+//  Copyright © 2017年 MF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
